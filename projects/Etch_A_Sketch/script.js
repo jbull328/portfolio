28d9fbc50b2bc4f029acb2c5bac116638d16923b
John T. Bull <jackbull328@gmail.com>
2015-07-13 22:16:03 -0700

    fixed sketch colors

--- projects/Etch_A_Sketch/script.js
+++ projects/Etch_A_Sketch/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-	
-	
-	
-	for (var  i=0; i <16; i++) 
+
+
+
+	for (var  i=0; i <16; i++)
 	{
-	
+
 		$('div').append('<div class="box"></div>' + i)
-	
+
 	}
-	
-	
+
+
 });
